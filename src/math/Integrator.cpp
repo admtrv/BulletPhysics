d2e9ee1c9761cf6854830a03cd15a2febfe2f8b9
Anton Dmitriev <tosha.dmitriefff@yandex.ru>
2026-03-02 16:47:18 +0100

    refator: integrators 0 allocations

--- src/math/Integrator.cpp
+++ src/math/Integrator.cpp
@@ -7,6 +7,27 @@
 namespace BulletPhysics {
 namespace math {
 
+// calculate acceleration at given state
+Vec3 calcAccel(dynamics::IPhysicsBody& body, dynamics::PhysicsWorld* world, double dt, const Vec3& pos, const Vec3& vel)
+{
+    body.setPosition(pos);
+    body.setVelocity(vel);
+    body.clearForces();
+
+    if (world)
+    {
+        world->applyForces(body, dt);
+    }
+
+    Vec3 a = {0, 0, 0};
+    if (body.getMass() > 0.0)
+    {
+        a = body.getAccumulatedForces() / body.getMass();
+    }
+
+    return a;
+}
+
 void EulerIntegrator::step(dynamics::IPhysicsBody& body, dynamics::PhysicsWorld* world, double dt)
 {
     // clear previous forces
@@ -42,37 +63,14 @@ void MidpointIntegrator::step(dynamics::IPhysicsBody& body, dynamics::PhysicsWor
     const Vec3 r0 = body.getPosition();
     const Vec3 v0 = body.getVelocity();
 
-    // helper lambda to calculate acceleration at given state
-    auto calcAccel = [&](const Vec3& pos, const Vec3& vel) -> Vec3
-    {
-        // temporarily set state
-        auto tempBody = body.clone();
-        tempBody->setPosition(pos);
-        tempBody->setVelocity(vel);
-        tempBody->clearForces();
-
-        if (world)
-        {
-            world->applyForces(*tempBody, dt);
-        }
-
-        Vec3 a = {0, 0, 0};
-        if (tempBody->getMass() > 0.0)
-        {
-            a = tempBody->getAccumulatedForces() / tempBody->getMass();
-        }
-
-        return a;
-    };
-
     // RK2 (Midpoint) steps
-    Vec3 a0 = calcAccel(r0, v0);
+    Vec3 a0 = calcAccel(body, world, dt, r0, v0);
 
     const Vec3 k1_v = a0 * dt;
     const Vec3 k1_r = v0 * dt;
 
     // evaluate at midpoint
-    Vec3 a_mid = calcAccel(r0 + k1_r * 0.5, v0 + k1_v * 0.5);
+    Vec3 a_mid = calcAccel(body, world, dt, r0 + k1_r * 0.5, v0 + k1_v * 0.5);
     const Vec3 k2_v = a_mid * dt;
     const Vec3 k2_r = (v0 + k1_v * 0.5) * dt;
 
@@ -93,44 +91,21 @@ void RK4Integrator::step(dynamics::IPhysicsBody& body, dynamics::PhysicsWorld* w
     const Vec3 r0 = body.getPosition();
     const Vec3 v0 = body.getVelocity();
 
-    // helper lambda to calculate acceleration at given state
-    auto calcAccel = [&](const Vec3& pos, const Vec3& vel) -> Vec3
-    {
-        // temporarily set state
-        auto tempBody = body.clone();
-        tempBody->setPosition(pos);
-        tempBody->setVelocity(vel);
-        tempBody->clearForces();
-
-        if (world)
-        {
-            world->applyForces(*tempBody, dt);
-        }
-
-        Vec3 a = {0, 0, 0};
-        if (tempBody->getMass() > 0.0)
-        {
-            a = tempBody->getAccumulatedForces() / tempBody->getMass();
-        }
-
-        return a;
-    };
-
     // RK4 steps
-    Vec3 a0 = calcAccel(r0, v0);
+    Vec3 a0 = calcAccel(body, world, dt, r0, v0);
 
     const Vec3 k1_v = a0 * dt;
     const Vec3 k1_r = v0 * dt;
 
-    Vec3 a1 = calcAccel(r0 + k1_r * 0.5, v0 + k1_v * 0.5);
+    Vec3 a1 = calcAccel(body, world, dt, r0 + k1_r * 0.5, v0 + k1_v * 0.5);
     const Vec3 k2_v = a1 * dt;
     const Vec3 k2_r = (v0 + k1_v * 0.5) * dt;
 
-    Vec3 a2 = calcAccel(r0 + k2_r * 0.5, v0 + k2_v * 0.5);
+    Vec3 a2 = calcAccel(body, world, dt, r0 + k2_r * 0.5, v0 + k2_v * 0.5);
     const Vec3 k3_v = a2 * dt;
     const Vec3 k3_r = (v0 + k2_v * 0.5) * dt;
 
-    Vec3 a3 = calcAccel(r0 + k3_r, v0 + k3_v);
+    Vec3 a3 = calcAccel(body, world, dt, r0 + k3_r, v0 + k3_v);
     const Vec3 k4_v = a3 * dt;
     const Vec3 k4_r = (v0 + k3_v) * dt;
 
